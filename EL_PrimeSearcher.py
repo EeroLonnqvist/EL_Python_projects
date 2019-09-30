39addbbc5f4f33c7799ce7325a99b62d9fcf1101
Eero Lönnqvist <48858759+EeroLonnqvist@users.noreply.github.com>
2019-09-30 12:31:07 +0200

    Add files via upload

---
+++ EL_PrimeSearcher.py
@@ -0,0 +1,14 @@
+#This program asks the user for an integer and searches the next prime number
+print ("Enter an integer:")
+user_prompt=int(input())
+possible_prime=user_prompt+1
+tester=True
+def prime_check(truthy, integer):
+    for i in range(2, integer-1):
+        if integer%i==0:
+            truthy=False
+    return truthy
+while not prime_check(tester, possible_prime):
+    possible_prime=possible_prime+1
+else:
+    print ("The next prime number is: {0}".format(possible_prime))
