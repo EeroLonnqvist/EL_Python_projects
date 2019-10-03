73a6ccb713ac6ab2ee6b19a1bed4d3fc409d671e
Eero Lönnqvist <48858759+EeroLonnqvist@users.noreply.github.com>
2019-10-03 13:04:36 +0200

    Add files via upload

---
+++ EL_Codecademy_TIPPython_List_P1.py
@@ -0,0 +1,18 @@
+# rotate list
+# no time/space requirements
+# return "rotated" version of input list
+
+def rotate(my_list, num_rotations):
+    empty_list = [0]*len(my_list)
+    for i in range(0, len(my_list)):
+        empty_list[(i+num_rotations)%len(my_list)]=my_list[i]
+    return empty_list
+
+#### TESTS SHOULD ALL BE TRUE ####
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1), ['f', 'a', 'b', 'c', 'd', 'e'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1) == ['f', 'a', 'b', 'c', 'd', 'e']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2), ['e', 'f', 'a', 'b', 'c', 'd'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2) == ['e', 'f', 'a', 'b', 'c', 'd']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3), ['d', 'e', 'f', 'a', 'b', 'c'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3) == ['d', 'e', 'f', 'a', 'b', 'c']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4), ['c', 'd', 'e', 'f', 'a', 'b'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4) == ['c', 'd', 'e', 'f', 'a', 'b']))
